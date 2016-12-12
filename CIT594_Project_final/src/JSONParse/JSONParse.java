c8600939b40f357b00df3762de6f9f82497239f2
BernieSanders <fankejin@seas.upenn.edu>
2016-12-12 14:23:15 -0500

    Add comments to JSONParse

--- CIT594_Project_final/src/JSONParse/JSONParse.java
+++ CIT594_Project_final/src/JSONParse/JSONParse.java
@@ -3,6 +3,7 @@ import org.json.simple.*;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 public class JSONParse {
+	// parse a string and return a json obj corresponding to this string
 	public static JSONObject getJSON(String string) {
 		JSONParser parser = new JSONParser();
 		JSONObject response = null;
